24462669176eb1216dd4f3e2c77e257b0ffff46e
etube-code <english.tube@yandex.com>
2026-01-12 22:19:59 +0300

    stream part 1 00:22:51

--- src/com/mojang/escape/EscapeComponent.java
+++ src/com/mojang/escape/EscapeComponent.java
@@ -1,7 +1,21 @@
 package com.mojang.escape;
 
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 
 public class EscapeComponent extends JComponent {
+	private static final long serialVersionUID = 1L;
+	
+	private static final int WIDTH=160;
+	private static final int HEGIHT=120;
+	private static final int SCALE=4;
+	
+	
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame("Escape!");
+		
+		
+	}
 
 }
