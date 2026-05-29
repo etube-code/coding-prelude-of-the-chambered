1e48f3c63013ca70d1d12e203a0a2d79231866d4
etube-code <english.tube@yandex.com>
2026-05-29 22:16:55 +0300

    stream part 1 01:36:00 - wall adjustments 1

--- src/com/mojang/escape/gui/Bitmap3D.java
+++ src/com/mojang/escape/gui/Bitmap3D.java
@@ -21,7 +21,7 @@ public class Bitmap3D extends Bitmap {
 		double yCam = game.time % 100 / 100.0;
 		double zCam = 0;
 
-		double rot = 0;
+		double rot = Math.sin(game.time/10.0) * 0.5;
 
 		double rCos = Math.cos(rot);
 		double rSin = Math.sin(rot);
@@ -39,8 +39,8 @@ public class Bitmap3D extends Bitmap {
 				double xd = (x - width / 2.0) / height;
 				xd *= zd;
 
-				double xx = (xd * rCos + zd * rSin + (xCam + 0.5) * 8);
-				double yy = (zd * rCos - xd * rSin + (yCam) * 8);
+				double xx = xd * rCos + zd * rSin + (xCam + 0.5) * 8;
+				double yy = zd * rCos - xd * rSin + (yCam) * 8;
 
 				int xPix = (int) (xx);
 				int yPix = (int) (yy);
@@ -57,13 +57,17 @@ public class Bitmap3D extends Bitmap {
 
 		Random random = new Random(100);
 		for (int i = 0; i < 1000; i++) {
-			double xx = random.nextDouble() * 2 - 1;
-			double yy = random.nextDouble() * 2 - 1;
-			double zz = 2 - yCam;
+			double x = random.nextDouble() * 2 - 1;
+			double y = random.nextDouble() * 2 - 1;
+			double z = 2;
+
+			double xx = x * rCos - z * rSin +xCam;
+			double zz = z * rCos + x * rSin  - yCam;
+			double yy = y;			
 			int xPixel = (int) (xx / zz * height / 2 + width / 2);
 			int yPixel = (int) (yy / zz * height / 2 + height / 2);
 			if (xPixel >= 0 && yPixel >= 0 && xPixel < width && yPixel < height) {
-
+				zBuffer[xPixel + yPixel * width] = zz;
 				pixels[xPixel + yPixel * width] = 0xff00ff;
 			}
 
