14e63db2d8cfd1d668e81be05a49537bb756d331
etube-code <english.tube@yandex.com>
2026-05-29 22:56:06 +0300

    stream part 1 01:41:17 - wall adjustments 2

--- src/com/mojang/escape/gui/Bitmap3D.java
+++ src/com/mojang/escape/gui/Bitmap3D.java
@@ -18,10 +18,10 @@ public class Bitmap3D extends Bitmap {
 	public void render(Game game) {
 
 		double xCam = 0;
-		double yCam = game.time % 100 / 100.0;
+		double yCam = game.time % 100 / 20.0 - 2;
 		double zCam = 0;
 
-		double rot = Math.sin(game.time/10.0) * 0.5;
+		double rot = Math.sin(game.time / 40.0) * 0.5;
 
 		double rCos = Math.cos(rot);
 		double rSin = Math.sin(rot);
@@ -29,10 +29,10 @@ public class Bitmap3D extends Bitmap {
 		for (int y = 0; y < height; y++) {
 			double yd = ((y + 0.5) - height / 2.0) / height;
 
-			double zd = (4 + zCam) / yd;
+			double zd = (8 + zCam) / yd;
 
 			if (yd < 0) {
-				zd = (4 - zCam) / -yd;
+				zd = (8 - zCam) / -yd;
 			}
 
 			for (int x = 0; x < width; x++) {
@@ -42,8 +42,8 @@ public class Bitmap3D extends Bitmap {
 				double xx = xd * rCos + zd * rSin + (xCam + 0.5) * 8;
 				double yy = zd * rCos - xd * rSin + (yCam) * 8;
 
-				int xPix = (int) (xx);
-				int yPix = (int) (yy);
+				int xPix = (int) (xx / 2 + 2);
+				int yPix = (int) (yy / 2 + 2);
 				if (xx < 0)
 					xPix--;
 				if (yy < 0)
@@ -57,20 +57,23 @@ public class Bitmap3D extends Bitmap {
 
 		Random random = new Random(100);
 		for (int i = 0; i < 1000; i++) {
-			double x = random.nextDouble() * 2 - 1;
-			double y = random.nextDouble() * 2 - 1;
-			double z = 2;
+			double x = random.nextDouble() * 2 - 1 - xCam;
+			double z = random.nextDouble() * 2 - 1 - zCam;
+			double y = 2 - yCam;
 
-			double xx = x * rCos - z * rSin +xCam;
-			double zz = z * rCos + x * rSin  - yCam;
-			double yy = y;			
-			int xPixel = (int) (xx / zz * height / 2 + width / 2);
-			int yPixel = (int) (yy / zz * height / 2 + height / 2);
-			if (xPixel >= 0 && yPixel >= 0 && xPixel < width && yPixel < height) {
-				zBuffer[xPixel + yPixel * width] = zz;
-				pixels[xPixel + yPixel * width] = 0xff00ff;
+			double xx = x * rCos - y * rSin;
+			double yy = z;
+			double zz = y * rCos + x * rSin;
+
+			if (zz > 0) {
+
+				int xPixel = (int) (xx / zz * height + width / 2);
+				int yPixel = (int) (yy / zz * height + height / 2);
+				if (xPixel >= 0 && yPixel >= 0 && xPixel < width && yPixel < height) {
+					zBuffer[xPixel + yPixel * width] = zz * 5;
+					pixels[xPixel + yPixel * width] = 0xffffff;
+				}
 			}
-
 		}
 	}
 
